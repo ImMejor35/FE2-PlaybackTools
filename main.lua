04a3c59e04742e96a7a931db08f7537208516a4b
tomato.txt <104869883+ImMejor35@users.noreply.github.com>
2023-09-04 13:43:47 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,10 +2,4 @@
 local function HttpLoad(url)
  return loadstring(game:HttpGetAsync(url))
 end
-local function SafeCall(GameFunc, ...)
-  setthreadcontext(2)
-  local Beep = GameFunc(...)
-  setthreadcontext(8)
-  return Beep
-end
 HttpLoad("https://raw.github.com/ImMejor35/FE2-PlaybackTools/tested/prep.lua")()
